90f534374d4b593b5698654e8bb97fc2df7988c0
Sivaranjani Rajasekaran <sivaranjani.r@syncfusion.com>
2026-05-12 19:03:34 +0530

    update the sample

--- connecting-to-adaptors/UrlAdaptor/UrlAdaptor.client/src/App.jsx
+++ connecting-to-adaptors/UrlAdaptor/UrlAdaptor.client/src/App.jsx
@@ -18,10 +18,10 @@ function App() {
 
     // Configure DataManager with UrlAdaptor.
     const data = new DataManager({
-        url: 'http://localhost:5123/api/data',  // Replace 7007 with the backend port.
-        insertUrl: 'http://localhost:5123/api/data/Insert',
-        updateUrl: 'http://localhost:5123/api/data/Update',
-        removeUrl: 'http://localhost:5123/api/data/Remove',
+        url: 'http://localhost:7007/api/data',  // Replace 7007 with the backend port.
+        insertUrl: 'http://localhost:7007/api/data/Insert',
+        updateUrl: 'http://localhost:7007/api/data/Update',
+        removeUrl: 'http://localhost:7007/api/data/Remove',
         adaptor: new UrlAdaptor()                // Specify UrlAdaptor for custom REST API.
     });
 
